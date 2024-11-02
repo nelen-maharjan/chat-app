5c98cca716eaef23d3175a85ff577d6b03b37f3f
Nelen Maharjan <neleen11@gmail.com>
2024-11-02 19:22:37 +0545

    user-list & chat-component design

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -2,7 +2,24 @@ import './chat.css';
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="avatar" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="phone-icon" />
+          <img src="./video.png" alt="video-icon" />
+          <img src="./info.png" alt="info-icon" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom"></div>
+    </div>
   )
 }
 
--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,9 +1,46 @@
-import './chatList.css'
+import { useState } from "react";
+import "./chatList.css";
 
 const ChatList = () => {
-  return (
-    <div>ChatList</div>
-  )
-}
+  const [addMode, setAddMode] = useState(false);
 
-export default ChatList
+  return (
+    <div className="chatList">
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="search" />
+          <input type="text" placeholder="Search" />
+        </div>
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          className="add"
+          onClick={() => setAddMode((prev) => !prev)}
+          alt="plus-icon"
+        />
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="user-img" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="user-img" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="user-img" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatList;
