6abf61857ac5ed800396f48cf7e016f50601cc78
Nelen Maharjan <neleen11@gmail.com>
2024-11-03 19:13:48 +0545

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import Chat from "./components/chat/Chat"
-import Detail from "./components/detail/Detail"
-import List from "./components/list/List"
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+import List from "./components/list/List";
+import Login from "./components/login/Login";
 
 function App() {
+  const user = false;
 
   return (
     <div className="container">
-      <List />
-      <Chat />
-      <Detail />
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
