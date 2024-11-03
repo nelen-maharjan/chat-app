0430671b37d9c519dc4d7935aabc4e9e8ff5a579
Nelen Maharjan <neleen11@gmail.com>
2024-11-03 18:58:25 +0545

    chat-detail design

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,49 @@ import './detail.css';
 
 const Detail = () => {
   return (
-    <div className='detail'>Detail</div>
+    <div className='detail'>
+      <div className="user">
+        <img src="./avatar.png" alt="user-img" />
+        <h2>Jane Doe</h2>
+        <p>Lorem ipsum dolor, sit amet</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="arrow" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & help</span>
+            <img src="./arrowUp.png" alt="arrow" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="arrow" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+              <img src="https://pbs.twimg.com/media/Fn5qjz9WQAAXUgE.jpg" alt="img" />
+              <span>photo_2024.png</span>
+              </div>
+            <img className='icon' src="./download.png" alt="download-img" />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="arrow" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
   )
 }
 
