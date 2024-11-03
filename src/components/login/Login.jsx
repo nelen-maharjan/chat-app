f0d21fcda45115b97ab56c04e87cf676b4520c78
Nelen Maharjan <neleen11@gmail.com>
2024-11-03 19:21:59 +0545

    upload img

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,48 @@
-import './login.css'
+import { useState } from "react";
+import "./login.css";
 
 const Login = () => {
-  return (
-    <div className='login'>Login</div>
-  )
-}
+    const [avatar, setAvatar] = useState({
+        file:null,
+        url:'',
+    });
 
-export default Login
+    const handleAvatar = (e) =>{
+        if(e.target.files[0]){
+            setAvatar({
+                file:e.target.files[0],
+                url: URL.createObjectURL(e.target.files[0])
+            })
+        }
+    }
+
+  return (
+    <div className="login">
+      <div className="item">
+        <h2>Welcome back,</h2>
+        <form>
+          <input type="text" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <button>Sign In</button>
+        </form>
+      </div>
+      <div className="seperator"></div>
+      <div className="item">
+        <h2>Create an Account</h2>
+        <form>
+          <label htmlFor="file">
+            <img src={avatar.url || './avatar.png'} alt="user-img" />
+            Upload an image
+          </label>
+          <input type="file" id="file" style={{ display: "none" }} onChange={handleAvatar} />
+          <input type="text" placeholder="Username" name="username" />
+          <input type="text" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <button>Sign In</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
