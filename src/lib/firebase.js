04f047e6a618df939495852cd3deeabc64fdb042
Nelen Maharjan <neleen11@gmail.com>
2024-11-05 20:54:25 +0545

    appwrite setup

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,5 +1,6 @@
-
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,3 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth()
+export const db = getFirestore()
